5a1f4c7d08948c9e6de0444cc8f9490f9dda551c
TristanDiabolo <tristan@diabolo-web.com>
2021-04-17 16:52:21 +0200

    Add migration

---
+++ migrations/Version20210417145149.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210417145149 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE record (id INT AUTO_INCREMENT NOT NULL, song_id INT DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, archived_at DATETIME DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_9B349F91A0BDB2F3 (song_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE song (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE record ADD CONSTRAINT FK_9B349F91A0BDB2F3 FOREIGN KEY (song_id) REFERENCES song (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE record DROP FOREIGN KEY FK_9B349F91A0BDB2F3');
+        $this->addSql('DROP TABLE record');
+        $this->addSql('DROP TABLE song');
+    }
+}

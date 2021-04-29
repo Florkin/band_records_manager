e47267259e25c8e38e32ad85a8547691ef49bd20
TristanDiabolo <tristan@diabolo-web.com>
2021-04-29 09:24:37 +0200

    ignore .idea

---
+++ assets/fileDrop.js
@@ -0,0 +1,18 @@
+// import Dropzone from "dropzone";
+// import Amplitude from "amplitudejs";
+//
+// Dropzone.autoDiscover = false;
+//
+// const dropzoneElem = document.getElementById('file-dropzone');
+// const fileDrop = new Dropzone('#file-dropzone', {
+//     url: dropzoneElem.getAttribute('action'),
+//     acceptedFiles: 'audio/aac, audio/mpeg, .mp3, .aac',
+//     init: function() {
+//         this.on("success", function(file) {
+//             alert('ni');
+//             const record = JSON.parse(JSON.parse(file.xhr.responseText));
+//             // Amplitude.addSong(record);
+//         });
+//     }
+// });
+
